3e36d0f42779b38213492743717a4845a1cb98ca
raj <raj@raj-300E4C-300E5C-300E7C>
2016-12-29 10:07:24 +0530

    API Done: 1) Get Customer List. 2) Create Customer. 3) Get Customer Details. 4) Update Customer. 5) Delete Customer.

---
+++ Smart Quote/WEB-INF/src/pojo/CustomerBean.java
@@ -0,0 +1,122 @@
+package pojo;
+
+public class CustomerBean {
+	private String customerCode;
+	private String customerName;
+	private String state;
+	private String postalCode;
+	private String address1;
+	private String address2;
+	private String phone;
+	private String contactPerson;
+	private String fax;
+	private String email;
+	private int totalStaff;
+	private String avgPurchase;
+	private String industryType;
+
+	public String getCustomerCode() {
+		return customerCode;
+	}
+
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getContactPerson() {
+		return contactPerson;
+	}
+
+	public void setContactPerson(String contactPerson) {
+		this.contactPerson = contactPerson;
+	}
+
+	public String getFax() {
+		return fax;
+	}
+
+	public void setFax(String fax) {
+		this.fax = fax;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getTotalStaff() {
+		return totalStaff;
+	}
+
+	public void setTotalStaff(int totalStaff) {
+		this.totalStaff = totalStaff;
+	}
+
+	public String getAvgPurchase() {
+		return avgPurchase;
+	}
+
+	public void setAvgPurchase(String avgPurchase) {
+		this.avgPurchase = avgPurchase;
+	}
+
+	public String getIndustryType() {
+		return industryType;
+	}
+
+	public void setIndustryType(String industryType) {
+		this.industryType = industryType;
+	}
+
+}
