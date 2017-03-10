dfcf4765a287c7e16c61bffaa134d13dbc5db9da
pradnyagade <pradnya@giantleapsystems.com>
2017-03-10 15:03:02 +0530

    c

--- Smart Quote/WEB-INF/src/action/CustComparisonAction.java
+++ Smart Quote/WEB-INF/src/action/CustComparisonAction.java
@@ -1,7 +1,10 @@
 package action;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+
+import pojo.QuoteBean;
 
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -9,7 +12,17 @@ public class CustComparisonAction extends ActionSupport{
 	
 	Map<String ,String > exportParameters ;
 	ArrayList<String> list = new ArrayList<String>();
+	QuoteBean objQuoteBean ;
+	int quoteId;
 	
+	
+	
+	public int getQuoteId() {
+		return quoteId;
+	}
+	public void setQuoteId(int quoteId) {
+		this.quoteId = quoteId;
+	}
 	public Map<String, String> getExportParameters() {
 		return exportParameters;
 	}
@@ -22,7 +35,19 @@ public class CustComparisonAction extends ActionSupport{
 	public void setList(ArrayList<String> list) {
 		this.list = list;
 	}
-
+	public QuoteBean getObjQuoteBean() {
+		return objQuoteBean;
+	}
+	public void setObjQuoteBean(QuoteBean objQuoteBean) {
+		this.objQuoteBean = objQuoteBean;
+	}
+	
+	public String exportPDF(){
+		System.out.println("Quote Id : "+quoteId);
+		objQuoteBean = new QuoteBean();
+		exportParameters = new HashMap<String, String>();
+		return SUCCESS;
+	}
 	
 	
 	
