dbcceaf8489684750541b403bf6c17bacbb9666b
radhikaudapure <radhika@giantleapsystems.com>
2018-02-22 14:29:46 +0530

    payment reminder (.xml uploading) QA version

--- SmartQuote/SQ/SQApp.js
+++ SmartQuote/SQ/SQApp.js
@@ -10,7 +10,7 @@ var app= angular.module('sq.SmartQuoteDesktop',['ui.router','ui.bootstrap','ngSa
    $rootScope.projectName="/";
 
      var currentURL=$window.location.href; 
-// var currentURL="http://localhost:6003/smartprotest/"; 
+ var currentURL="http://localhost:6003/smartprotest/"; 
 
    var isSmartProTest=currentURL.includes("smartprotest");
    if (isSmartProTest) {
