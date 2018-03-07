d00d77f7f4b19a699fc742e02d810a638599be1b
radhikaudapure <radhika@giantleapsystems.com>
2018-03-07 13:48:11 +0530

    pdf changes

---
+++ SmartQuote/WEB-INF/src/pojo/PdfPageBean.java
@@ -0,0 +1,14 @@
+package pojo;
+
+public class PdfPageBean {
+private String pageTemplateSrc;
+
+public String getPageTemplateSrc() {
+	return pageTemplateSrc;
+}
+
+public void setPageTemplateSrc(String pageTemplateSrc) {
+	this.pageTemplateSrc = pageTemplateSrc;
+}
+
+}
