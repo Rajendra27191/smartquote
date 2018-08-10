2b4712224f0db089c4799c04485780fe397354a1
radhikaudapure <radhika@giantleapsystems.com>
2018-08-10 11:00:57 +0530

    done

--- SmartQuote/SQ/UserHome/Quote/js/SQAutoSaveQuoteConroller.js
+++ SmartQuote/SQ/UserHome/Quote/js/SQAutoSaveQuoteConroller.js
@@ -914,7 +914,7 @@ angular.module('sq.SmartQuoteDesktop')
                     'offerList': $scope.offerArray,
                     'userId': $rootScope.userData.userId,
                     'isNewProductAdded': isNewProductAdded,
-                    'isSaveAndPrint': $scope.isSaveAndPrintInitiated,
+                    // 'isSaveAndPrint': $scope.isSaveAndPrintInitiated,
                 }
                 return objQuoteBean;
             }
